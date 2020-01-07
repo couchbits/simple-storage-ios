178667bc53ee4f5f4661eefe26296d4b21eb33d9
Dominik Gauggel <dominik@couchbits.com>
2020-01-07 14:37:52 +0100

    Adds Carthage support

---
+++ SimpleStorage/SimpleStorage.h
@@ -0,0 +1,18 @@
+//
+//  SimpleStorage.h
+//  SimpleStorage
+//
+//  Created by Dominik Gauggel on 07.01.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleStorage.
+FOUNDATION_EXPORT double SimpleStorageVersionNumber;
+
+//! Project version string for SimpleStorage.
+FOUNDATION_EXPORT const unsigned char SimpleStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleStorage/PublicHeader.h>
+
+
